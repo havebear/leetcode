c20050cecb05bdbfd4b1e086d7fcbfdbe19f8182
havebear <616749285@qq.com>
2020-09-02 16:35:30 +0800

    新增题目：一维数组的动态和

---
+++ 0006_有效的括号/20200402.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: 616749285@qq.com
+ * @Date: 2020-04-02 12:55:29
+ * @LastEditors: 616749285@qq.com
+ * @LastEditTime: 2020-04-02 12:56:06
+ * @Description:  有效的括号
+ * https://leetcode-cn.com/problems/valid-parentheses/
+ */
+
+var map = new Map([
+  ['(', ')'],
+  ['{', '}'],
+  ['[', ']']
+])
+
+/**
+* @param {string} s
+* @return {boolean}
+*/
+var isValid = function(s) {
+  if (!s) return true
+  if (s.length < 2) return false
+  var temp = []
+  for (var i = 0; s[i] != null; i++) {
+      if (temp.length && map.get(temp[temp.length - 1]) === s[i]) {
+          temp.pop()
+      } else {
+          temp.push(s[i])
+      }
+  }
+  return !temp[0]
+};
+
+isValid('()')
---
+++ 0007_ 一维数组的动态和/20200402.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: 616749285@qq.com
+ * @Date: 2020-04-02 12:55:29
+ * @LastEditors: 616749285@qq.com
+ * @LastEditTime: 2020-09-02 16:35:06
+ * @Description:  一维数组的动态和
+ * https://leetcode-cn.com/problems/running-sum-of-1d-array/
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+  const length = nums.length
+  for (let i = 0; i < length; i++) {
+      nums[i] += nums[i - 1] || 0
+  }
+  return nums
+};
+
+runningSum([1,2,3,4])
