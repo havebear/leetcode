e8f766a0f2a93b18c119853b26f6b72b3f04bdf4
havebear <616749285@qq.com>
2021-06-03 14:42:10 +0800

    chore: 增加【实现strStr()】

---
+++ 0012_实现 strStr()/20210603.js
@@ -0,0 +1,37 @@
+/*
+ * @Author: 616749285@qq.com
+ * @Date: 2021-06-03 10:45:31
+ * @LastEditors: 616749285@qq.com
+ * @LastEditTime: 2021-06-03 14:41:31
+ * @Description:  实现 strStr()
+ * https://leetcode-cn.com/problems/implement-strstr/
+ */
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+ var strStr = function(haystack, needle) {
+    if (needle === '' || haystack === needle) return 0
+    if (haystack.length < needle.length) return -1
+
+    const haystackLength = haystack.length
+    const needleLength = needle.length
+
+    for (let i = 0; i < haystackLength; i++) {
+        if ((haystackLength - i) < needleLength) break
+        if (haystack[i] === needle[0]) {
+            let isFind = true
+            for (let j = 0; j < needleLength; j++) {
+                if (haystack[i + j] !== needle[j]) {
+                    isFind = false
+                    break
+                }
+            }
+            if (isFind) return i
+        }
+    }
+
+    return -1
+};
